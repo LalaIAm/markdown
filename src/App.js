075d405c93687962d68d0a08e63f3d9639b5242e
LalaIAm <lthorud12@gmail.com>
2022-11-04 23:28:09 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
 
-function App() {
+import {marked} from 'marked'
+import { sampleText } from './sampleText'
+
+const App = () => {
+  const [text, setText] = useState(sampleText)
+
+
+
+  const renderText = text => {
+    const __html = marked(text, {sanitize: true})
+    return {__html}
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-sm-6'>
+          <textarea onChange={e => setText(e.target.value)} id='editor' className='form-control' value={text} rows='35' />
+        </div>
+        <div className='col-sm-6'>
+<div id='preview' dangerouslySetInnerHTML={renderText(text)}></div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
